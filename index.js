920dda1b3d10837341e9bb6d314e4d09d9bdd6f3
Marisol Davila Escobar <marisol.davila@correounivalle.edu.co>
2018-07-11 12:27:03 -0500

    OVA ACTUAL
    OVA con las ultimas mdificaciones

---
+++ index.js
@@ -0,0 +1,67 @@
+/*(function(){
+  $('.menu-left').click(function(){
+    
+    $('.intro').toggleClass('active')
+  /*   $('section').toggleClass('active')
+    $('#menu-left').toggleClass('active')
+   
+  })
+ 
+})()
+$('.menu-left').on('click', function() {
+	$('.intro').toggleClass('abrir');
+});*/
+
+$(function() {
+  var Accordion = function(el, multiple) {
+    this.el = el || {};
+    this.multiple = multiple || false;
+
+    // Variables privadas
+    var links = this.el.find(".content h5");
+    // Evento
+    links.on("click", { el: this.el, multiple: this.multiple }, this.dropdown);
+  };
+
+  Accordion.prototype.dropdown = function(e) {
+    var $el = e.data.el;
+    ($this = $(this)), ($next = $this.next());
+
+    $next.slideToggle();
+    $this.parent().toggleClass("open");
+
+    if (!e.data.multiple) {
+      $el
+        .find(".content li")
+        .not($next)
+        .slideUp()
+        .parent()
+        .removeClass("open");
+    }
+  };
+
+  var accordion = new Accordion($("#menu-left"), true);
+});
+$(document).ready(function(e){
+  $('#itemObjetivos').on('click', function(){
+    $('#content').load('inicio.html')
+  });
+});
+$(document).ready(function(e){
+  $('#itemActividad').on('click', function(){
+    $('#content').load('jsLanguaje.html')
+  });
+});
+$(document).ready(function(e){
+  $('#itemTema1').on('click', function(){
+    $('#content').load('cssLanguaje.html')
+  });
+});
+
+$(document).ready(function() {
+  $('.has-animation').each(function(index) {
+    $(this).delay($(this).data('delay')).queue(function(){
+      $(this).addClass('animate-in');
+    });
+  });
+});
