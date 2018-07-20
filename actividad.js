a14c6aa1dbf16e19a55e961fa7600ecc9b8077f4
Marisol Davila Escobar <marisol.davila@correounivalle.edu.co>
2018-07-19 22:09:34 -0500

    Pestaña 2
    Efecto en proceso...

--- actividad.js
+++ actividad.js
@@ -1,36 +1,3 @@
-/*var bodyW = $(window).height();
-
-$('body').height(bodyW);
-
-$(window).resize(function() {
-  var bodyW = $(window).height();
-$('body').height(bodyM);  
-});*/
-
- $('.tabs a').click(function(){
-  $this = $(this);
- 
-
-  $('.panel').hide();
-  $('.tabs').removeClass('active').addClass('inactive');
-  $this.addClass('active').blur();
-  
-  var panelContainerColor = $this.css('background-color');
-
-  $('.panelContainer').css({backgroundColor: panelContainerColor});
-  
-  var panel = $this.attr('href');
-  
-  $(panel).fadeIn(350);
-  
-  return false;
- 
-});//end click
-
- 
-
-$('.tabs li:third a').click();
-
 $('.button').click(function(){
   var $btn = $(this),
       $step = $btn.parents('.modal-body'),
@@ -86,4 +53,3 @@ $('.rerun-button').click(function(){
                           .siblings().removeClass('is-active');
  $(this).hide();
 });
-
--- index.js
+++ index.js
@@ -65,3 +65,26 @@ $(document).ready(function() {
     });
   });
 });
+
+ (function(){
+            'use strict';
+
+            var $mainButton = $(".main-button"),
+                $closeButton = $(".close-button"),
+                $buttonWrapper = $(".button-wrapper"),
+                $ripple = $(".ripple"),
+                $layer = $(".layered-content");
+
+            $mainButton.on("click", function(){
+                $buttonWrapper.addClass("clicked").delay(900).queue(function(next){
+                    $layer.addClass("active");
+                    next();
+                });
+            });
+
+            $closeButton.on("click", function(){
+                $layer.removeClass("active");
+                $buttonWrapper.removeClass("clicked");
+            });
+            
+        })();
