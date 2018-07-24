b6f14c0bad2498994d106e32dcae21368c325d22
Marisol Davila Escobar <marisol.davila@correounivalle.edu.co>
2018-07-24 18:10:03 -0500

    Step 3

--- boton.js
+++ boton.js
@@ -16,3 +16,23 @@ function simulator_FB_close (){
    shadow.style.backgroundColor = "#FFF"
 
 }
+
+/* LIGHTBOX */
+
+$(document).ready(function () {
+    "use strict";
+    $(".lightbox").click(function () {
+        var imgsrc = $(this).attr('src');
+        $("body").append("<div class='img-popup'><span class='close-lightbox'>&times;</span><img src='" + imgsrc + "'></div>");
+        $(".close-lightbox, .img-popup").click(function () {
+            $(".img-popup").fadeOut(500, function () {
+                $(this).remove();
+            }).addClass("lightboxfadeout");
+        });
+
+    });
+    $(".lightbox").click(function () {
+        $(".img-popup").fadeIn(500);
+    });
+
+});
